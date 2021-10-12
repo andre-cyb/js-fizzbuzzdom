0a579918b9e1e2867891e6d53aa63f9bb4bd5539
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-12 17:22:22 +0200

    problemi con JS per aggiungere lo stile alle celle

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,5 @@
 const row = document.createElement("div");
 
-/* let divThreeStyle = document.querySelector(".row > div");
-divThreeStyle.style.display("bg-danger");
-*/
-/* h3.style.display("font-size"); */
 for (let i = 1; i <= 100; i++) {
 
     let box = document.querySelector(".box");
@@ -11,26 +7,35 @@ for (let i = 1; i <= 100; i++) {
     const h3 = document.createElement("h3");
 
     h3.textContent = i;
-    col.classList.add("col-7", "ratio", "ratio-1x1", "bg-light", "my_border", /* "d-flex", "justify-content-center", "align-items-middle" */);
+    col.classList.add("col-7", "ratio", "ratio-1x1", "my_border", "blue", /* "d-flex", "justify-content-center", "align-items-middle" */);
     row.classList.add("row");
 
     col.append(h3);
     row.append(col);
     box.append(row);
 
+    let prova = document.querySelector(".row div");
     let a = (i * 3);
+
+
+
+
 
     if (i % 3 === 0) {
         h3.textContent = ("Fizz");
-/*         col.style.display("bg-danger");
- */    }
+        /*prova.style.backgroundColor = ("#0bd6a1");*/
+    }
     if (i % 5 === 0) {
+        h3.textContent = ("Buzz");
+
 
     }
     if ((i % 3 === 0) && (i % 5 === 0)) {
         h3.textContent = ("FizzBuzz");
+        /*prova.style.backgroundColor = ("#f0466f");*/
 
     }
+
 
 
 
