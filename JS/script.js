3e11a915a4aaab241c8b4291e515115333d7c82e
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-12 15:16:36 +0200

    update

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,13 @@
+const row = document.createElement("div");
 
+for (let i = 1; i <= 10; i++) {
 
+    let box = document.querySelector(".box");
+    const col = document.createElement("div");
 
-for (let i = 1; i <= 100; i++) {
-
-
+    col.classList.add("col-7", "ratio", "ratio-1x1", "bg-light", "my_border");
+    row.classList.add("row");
+    row.append(col);
+    box.append(row);
 
 }
