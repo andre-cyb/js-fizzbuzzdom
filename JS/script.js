4a07cb00f6c44055a6b1da112a14374a8bc89324
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-12 15:23:50 +0200

    update celle numerate

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,15 @@
 const row = document.createElement("div");
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 100; i++) {
 
     let box = document.querySelector(".box");
     const col = document.createElement("div");
+    const h3 = document.createElement("h3");
 
+    h3.textContent = i;
     col.classList.add("col-7", "ratio", "ratio-1x1", "bg-light", "my_border");
     row.classList.add("row");
+    col.append(h3);
     row.append(col);
     box.append(row);
 
