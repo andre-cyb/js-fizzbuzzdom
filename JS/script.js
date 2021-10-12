c5fcbf37e70ce415858ac9b06f64bf4b1bcb65ca
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-12 15:45:29 +0200

    creati "Fizz" "Buzz" e "FizzBuzz"

--- JS/script.js
+++ JS/script.js
@@ -21,6 +21,13 @@ for (let i = 1; i <= 100; i++) {
         h3.textContent = ("Fizz");
 
     }
+    if (i % 5 === 0) {
+
+    }
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        h3.textContent = ("FizzBuzz");
+
+    }
 
 
 
